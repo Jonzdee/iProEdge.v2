c9a95c79d4e83b583099390ce33236245ba5fe02
Ogunyankin Johnson Olayemi <ogunyankinjohnson@gmail.com>
2025-06-23 06:13:03 +0100

    remove old pictures and designes

--- SeachBar/SearchBar.jsx
+++ SeachBar/SearchBar.jsx
@@ -1,21 +1,27 @@
 import { useState } from "react";
 import "./searchbar.css";
-import { products } from "../src/utils/products";
-// import useDebounce from "../../hooks/useDebounce";
-const SearchBar = ({ setFilterList }) => {
-  const [searchWord, setSearchWord] = useState(null);
-  // const debounceSearchWord = useDebounce(searchWord, 300);
-  const handelChange = (input) => {
-    setSearchWord(input.target.value);
+
+const SearchBar = ({ products, setFilterList }) => {
+  const [searchWord, setSearchWord] = useState("");
+
+  const handleChange = (input) => {
+    const value = input.target.value;
+    setSearchWord(value);
     setFilterList(
       products.filter((item) =>
-        item.productName?.toLowerCase().includes(searchWord?.toLowerCase())
+        item.productName?.toLowerCase().includes(value.toLowerCase())
       )
     );
   };
+
   return (
     <div className="search-container">
-      <input type="text" placeholder="Search..." onChange={handelChange} />
+      <input
+        type="text"
+        placeholder="Search..."
+        value={searchWord}
+        onChange={handleChange}
+      />
       <ion-icon name="search-outline" className="search-icon"></ion-icon>
     </div>
   );
