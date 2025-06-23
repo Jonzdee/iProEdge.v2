309ccc74091a57ce4f86610e85775c8669b6482b
Ogunyankin Johnson Olayemi <ogunyankinjohnson@gmail.com>
2025-06-23 07:20:55 +0100

    update on clear cart

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,21 +1,30 @@
 import { Col, Container, Row } from "react-bootstrap";
-;
+import bannerImage from '../../Images/speaker2.webp'
 import "./banner.css";
-const Banner = ({title}) => {
-    return ( 
-        <div className="image-container">
-          
-            <div className="overlay">
-                <Container>
-                    <Row>
-                        <Col>
-                            <h2>{title}</h2>
-                        </Col>
-                    </Row>
-                </Container>
-            </div>
-        </div>
-    );
-}
+const Banner = ({ title}) => {
+    
+  return (
+    <div
+      className="image-container"
+      style={{
+        backgroundImage: `url(${bannerImage})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        minHeight: "200px",
+        position: "relative"
+      }}
+    >
+      <div className="overlay">
+        <Container>
+          <Row>
+            <Col>
+              <h2>{title}</h2>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </div>
+  );
+};
 
 export default Banner;
